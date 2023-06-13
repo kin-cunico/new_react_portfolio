7360594cf8995d6547bdce6465047f0adce1131e
kin-cunico <124842064+kin-cunico@users.noreply.github.com>
2023-06-14 00:12:56 +0100

    commit with stars in index

--- new_portfolio/src/app/components/Navbar.tsx
+++ new_portfolio/src/app/components/Navbar.tsx
@@ -5,23 +5,42 @@ import Link from "next/link";
 import styles from "../styles/Navbar.module.css";
 import logo from "src/images/kin-cunico.png";
 import galaga from "src/images/galaga.png";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function Navbar() {
 	const [isOpen, setIsOpen] = useState(false);
+	const [isShot, setIsShot] = useState(false);
 	const handleClick = () => {
-		isOpen ? setIsOpen(!isOpen) : null;
+		setIsOpen(!isOpen);
+		console.log(isOpen);
 	};
 
-	const naveMove = () => {
-		isOpen === true ? "move" : "null";
-	};
-	const activeLink = () => {
-		document.getElementById("#0");
-	};
+	const navbarRef = useRef<HTMLDivElement>(null);
+	const shotRef = useRef("shot");
+
+	useEffect(() => {
+		if (navbarRef && navbarRef.current) {
+			navbarRef.current.addEventListener("click", (e) => {
+				console.log(e);
+				let x = e.pageX;
+				let y = e.pageY;
+				console.log(x, y);
+				let shot = document.createElement("div");
+				navbarRef.current?.appendChild(shot);
+				shot.className = `${styles.shot}`;
+				shot.style.left = `${x - 5}px`;
+				shot.style.top = `${y - 5}px`;
+			});
+		}
+		navbarRef.current?.removeEventListener("click", (e) => {});
+	}, [""]);
 
 	return (
-		<div className={styles.navbar}>
+		<div
+			ref={navbarRef}
+			className={styles.navbar}
+			id="navbar"
+		>
 			<style>
 				@import
 				url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
@@ -33,7 +52,7 @@ export default function Navbar() {
 				<div className={styles.wrapper}>
 					<div className={styles.link_wrapper}>
 						<Link
-							href="/pages"
+							href="/pages/index"
 							className={styles.link}
 							onClick={handleClick}
 							id="0"
