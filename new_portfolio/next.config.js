434074b0da8e0ed92c18b332c387cf950f8897ad
kin-cunico <124842064+kin-cunico@users.noreply.github.com>
2023-06-15 19:01:24 +0100

    with extensions_resolve

--- new_portfolio/next.config.js
+++ new_portfolio/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+	resolve: {
+		extensions: [".js", ".jsx"],
+	},
+};
